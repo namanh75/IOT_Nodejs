8bf0c88cac5b3a1d5f02e729a648701a82fe9496
Nam Anh <namanht96@gmail.com>
2022-06-21 22:38:29 +0700

    update mqtt

--- src/config/connect.js
+++ src/config/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/iot');
+        await mongoose.connect('mongodb://localhost:27017/iot')
         console.log('connect databse success!!!')
     } catch (error) {
         console.log('connect failure!!!')
